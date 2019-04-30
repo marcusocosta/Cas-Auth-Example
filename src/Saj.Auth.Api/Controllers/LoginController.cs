73d58f006fe5d7ae3eb2af104b50de1418c06c11
Marcus Oliveira Costa <marcus.costa@softplan.com.br>
2019-04-30 17:59:44 -0300

    Subindo o repositório

---
+++ src/Saj.Auth.Api/Controllers/LoginController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace Saj.Auth.Api.Controllers
+{
+    [Route("api")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        
+        public LoginController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        
+        [HttpGet("access-denied")]
+        public string AccessDenied()
+        {
+            return "Não autorizado";
+        }
+
+        [AllowAnonymous]
+        [HttpGet("login")]
+        public async Task Login(string returnUrl)
+        {
+            var request = Request.Cookies;
+            
+            var props = new AuthenticationProperties { RedirectUri = returnUrl };
+            await HttpContext.ChallengeAsync("CAS", props);
+        }
+        
+        [AllowAnonymous]
+        [HttpGet("signin")]
+        public string Signin()
+        {
+            return "";
+        }
+        
+        [Authorize]
+        [HttpGet("tokens")]
+        public string Tokens()
+        {
+            var claimsIdentity = User.Identity as ClaimsIdentity;
+            
+            
+            var request = Request.Cookies;
+            return "";
+        }
+        
+        [HttpGet("logout")]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync();
+            
+            return Redirect($"{_configuration["CasBaseUrl"]}/logout");
+            
+        }
+    }
+}
---
+++ src/Saj.Auth.Api/Startup.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using AspNetCore.Security.CAS;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Saj.Auth.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options =>
+                {
+                    options.LoginPath = new PathString("/login");
+                    options.AccessDeniedPath = new PathString("/access-denied");
+                    options.Cookie = new CookieBuilder
+                    {
+                        Name = ".AspNetCore.CasSample"
+                    };
+                })
+                .AddCAS(options =>
+                {
+                    options.CasServerUrlBase = Configuration["CasBaseUrl"];
+                    options.CallbackPath = new PathString("/api/signin");
+                    options.TicketValidator = new Cas2TicketValidator();
+                    options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                });
+                                                                  
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+        
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            
+            app.UseAuthentication();
+            app.UseMvc();
+        }
+    }
+}
